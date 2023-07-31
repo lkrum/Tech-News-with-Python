be8b43daea8db3e05d9ded9fe87a88625ecb9f09
lkrum <leahkrumholz73@gmail.com>
2023-07-31 17:22:59 -0500

    finally fixed the sql WHERE column error by removing brackets around the func.count function

--- seeds.py
+++ seeds.py
@@ -20,11 +20,11 @@ db.commit()
 
 # insert posts
 db.add_all([
-  Post(title='Donec posuere metus vitae ipsum', post_url='https://buzzfeed.com/in/imperdiet/et/commodo/vulputate.png', user_id=1),
-  Post(title='Morbi non quam nec dui luctus rutrum', post_url='https://nasa.gov/donec.json', user_id=1),
-  Post(title='Donec diam neque, vestibulum eget, vulputate ut, ultrices vel, augue', post_url='https://europa.eu/parturient/montes/nascetur/ridiculus/mus/etiam/vel.aspx', user_id=2),
-  Post(title='Nunc purus', post_url='http://desdev.cn/enim/blandit/mi.jpg', user_id=3),
-  Post(title='Pellentesque eget nunc', post_url='http://google.ca/nam/nulla/integer.aspx', user_id=4)
+  Post(title='My Favorite Ichiraku Ramens', post_url='https://buzzfeed.com/in/imperdiet/et/commodo/vulputate.png', user_id=1),
+  Post(title='Mastering Shadow Clone Jutsu', post_url='https://nasa.gov/donec.json', user_id=1),
+  Post(title='10 Steps to Obtain the Mangekyou Sharingan', post_url='https://europa.eu/parturient/montes/nascetur/ridiculus/mus/etiam/vel.aspx', user_id=2),
+  Post(title='Medical Ninjutsu 101', post_url='http://desdev.cn/enim/blandit/mi.jpg', user_id=3),
+  Post(title='Why Your Students Are Failing the Bell Test', post_url='http://google.ca/nam/nulla/integer.aspx', user_id=4)
 ])
 
 db.commit()
